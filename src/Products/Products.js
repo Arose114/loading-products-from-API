769ca44431608f4ab8dc3efc6b4709164328ec06
arose114 <akinolakemi114@gmail.com>
2024-10-07 11:36:26 +0100

    created the products component, fetched and rendered products from api

---
+++ src/Products/Products.js
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from 'react'
+import './style.css'
+
+const Products = () => {
+
+  const [loading, setLoading]=useState(false)
+  const [products, setProducts]=useState([])
+  const [count, setCount]=useState(0)
+
+async function fetchProducts() {
+
+  try {
+    setLoading(true)
+    const response= await fetch(`https://dummyjson.com/products?limit=20&skip=${count ===0 ? 0 : count +20}`)
+    const result= await response.json()
+    if (result && result.products && result.products.length){
+      setProducts(result.products)
+      setLoading(false)
+    }
+    
+  } catch (error) {
+    console.log(error.message)
+    setLoading(false)
+    
+  }
+  
+}
+
+  useEffect(()=>{
+    fetchProducts();
+
+  }, [])
+
+  if(loading){
+    return <div>Loading....please wait</div>
+  }
+
+  return (
+    <div className='products-wrapper'>
+      <h1 className="text">View List of Products!!!</h1>
+      <div className='product-container'>{
+        products && products.length? products.map((product,index)=><div className='products' key={index}>
+          <img src={product.thumbnail} alt={product.title} />
+          <p>{product.title}</p>
+
+        </div>)
+        
+        : null
+}</div>
+<div><button className='load-more'>Load More Products</button></div>
+    </div>
+  )
+}
+
+export default Products

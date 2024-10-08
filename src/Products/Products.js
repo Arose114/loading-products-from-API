1104bd176f2dc7ed9b04a03e095825edbd418fd0
arose114 <akinolakemi114@gmail.com>
2024-10-08 07:01:39 +0100

    .

--- src/Products/Products.js
+++ src/Products/Products.js
@@ -11,7 +11,7 @@ async function fetchProducts() {
 
   try {
     setLoading(true)
-    const response= await fetch(`https://dummyjson.com/products?limit=20&skip=${count ===0 ? 0 : count + 20}`)
+    const response= await fetch(`https://dummyjson.com/products?limit=20&skip=${count ===0 ? 0 : count +20}`)
     const result= await response.json()
     if (result && result.products && result.products.length){
       setProducts((prevProducts)=>[...prevProducts,...result.products])
